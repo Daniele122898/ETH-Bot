a7ac7bfbfe5d692f5f0de28d4ce16c10da8c0eb8
Daniele122898 <d.palbanese@gmail.com>
2018-09-17 21:43:36 +0200

    added Semester class, added modular scraper class

---
+++ ETH-Bot/ETH-Bot/Data/Entities/SubEntities/Semesters.cs
@@ -0,0 +1,22 @@
+using System.Collections.Generic;
+
+namespace ETH_Bot.Data.Entities.SubEntities
+{
+    public class Semesters
+    {
+        public List<Class> Classes { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class @Class
+    {
+        public string Name { get; set; }
+        public string[] Alias { get; set; }
+        public string Url { get; set; }
+        public string Xpath { get; set; }
+        public string Exercise { get; set; }
+        public string Solution { get; set; }
+        public bool HasExercise { get; set; }
+        public bool HasSolution { get; set; }
+    }
+}
--- ETH-Bot/ETH-Bot/Services/ScraperService.cs
+++ ETH-Bot/ETH-Bot/Services/ScraperService.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using ETH_Bot.Data.Entities.SubEntities;
 using HtmlAgilityPack;
 
@@ -6,6 +7,49 @@ namespace ETH_Bot.Services
 {
     public static class ScraperService
     {
+        public static List<Semesters> SemesterData = new List<Semesters>();
+
+        public static ScraperData Scrape(string url, string xpath, string ex, string sol, bool hasEx, bool hasSol)
+        {
+            var web = new HtmlWeb();
+            var doc = web.Load(url);
+            var nodes = doc.DocumentNode.SelectNodes(xpath);
+
+            if (nodes == null || nodes.Count == 0) return null;
+            
+            List<HtmlNode> exercises = new List<HtmlNode>();
+            List<HtmlNode> solutions = new List<HtmlNode>();
+
+            foreach (var node in nodes)
+            {
+                if (hasEx)
+                {
+                    if (node.InnerText.StartsWith(ex, StringComparison.OrdinalIgnoreCase))
+                    {
+                        exercises.Add(node);
+                        continue;
+                    }
+                }
+
+                if (hasSol)
+                {
+                    if (node.InnerText.StartsWith(sol, StringComparison.OrdinalIgnoreCase))
+                    {
+                        solutions.Add(node);
+                    }
+                }
+            }
+            
+            return new ScraperData()
+            {
+                Exercises = exercises,
+                Solutions = solutions
+            };
+            
+        }
+        
+        
+        
         public static ScraperData ScrapeLinAlg(string url)
         {
             var web = new HtmlWeb();
